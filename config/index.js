16511478d8a22cd03a5786801edc820c39851414
unknown <leidamin@adtime.com>
2017-09-01 09:54:02 +0800

    更改配置，解决图片不显示bug

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '/', //这里使用绝对路径，不然图片路径引入会出问题
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
